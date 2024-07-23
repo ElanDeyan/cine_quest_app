316eb97277aa54b9e235ce1ea7f22e14ce3871c5
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-07-23 19:16:24 -0300

    build: adds ensure widgets binding initialized

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cine_quest_app/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
