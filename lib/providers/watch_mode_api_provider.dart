1c74ab1399f39edaed7207149bde7b9931d1dffd
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-07-26 15:39:16 -0300

    refactor: adjusts and remove some methods

--- lib/providers/watch_mode_api_provider.dart
+++ lib/providers/watch_mode_api_provider.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:cine_quest_app/models/title_details.dart';
+import 'package:cine_quest_app/models/title_source.dart';
 import 'package:cine_quest_app/types/dict.dart';
 import 'package:cine_quest_app/types/json_map.dart';
 import 'package:dio/dio.dart';
@@ -20,108 +22,27 @@ final class WatchModeApiProvider extends ChangeNotifier {
         ),
       );
 
-  @override
-  Future<void> refresh() {
-    throw UnimplementedError();
-  }
-
-  // @override
-  // Future<List<JsonMap>> allTitlesPaginated({
-  //   int page = 1,
-  //   int manyPerPage = 50,
-  //   SortedByCriteria sortedBy = SortedByCriteria.relevanceDesc,
-  // }) async {
-  //   try {
-  //     final dataAsJson = jsonDecode(allTitlesPaginatedResponse);
-
-  //     if (dataAsJson case {"titles": final List<dynamic> titles}) {
-  //       return titles.map((item) => item as JsonMap).toList();
-  //     }
-
-  //     debugPrint('Some error occurred');
-  //     return const <JsonMap>[];
-  //     // final response = await http.get(
-  //     //   Uri.parse(
-  //     //     '$_baseUrl/list-titles/?apiKey=$_apiKey&page=$page&limit=$manyPerPage',
-  //     //   ),
-  //     // );
-
-  //     // if (response.statusCode == 200) {
-  //     //   final bodyAsJson =
-  //     //       jsonDecode(utf8.decode(response.bodyBytes)) as JsonMap;
-
-  //     //   if (bodyAsJson case {"titles": final List<dynamic> titles}) {
-  //     //     return titles
-  //     //         .map((item) => JsonMap(item as Map<String, dynamic>))
-  //     //         .toList();
-  //     //   }
-  //     // }
-
-  //     // return <JsonMap>[JsonMap.empty()];
-  //   } finally {
-  //     notifyListeners();
-  //   }
-  // }
-
   Future<Dict<List<JsonMap>>> autoCompleteSearch({
     int type = 2,
     required String query,
   }) async {
-    // final response = jsonDecode(autoCompleteSearchCarsResult);
-    // if (response case {"results": final List<dynamic> results}) {
-    //   return Dict(
-    //     {"results": results.map((item) => item as JsonMap).toList()},
-    //   );
-    // }
-    try {
-      final response = await _client.get<String>(
-        '/autocomplete-search/?apiKey=$_apiKey&search_type=$type&search_value=$query',
-      );
+    final response = await _client.get<String>(
+      '/autocomplete-search/?apiKey=$_apiKey&search_type=$type&search_value=$query',
+    );
 
-      if (response.statusCode == 200) {
-        print('-' * 10);
-        print('Inside provider');
-        final bodyAsJson = jsonDecode(response.data!) as JsonMap;
-        print('bodyAsJson: $bodyAsJson');
-        if (bodyAsJson case {"results": final List<dynamic> results}) {
-          return Dict({
-            "results": results
-                .map((result) => JsonMap(result as Dict<dynamic>))
-                .toList(),
-          });
-        }
+    if (response.statusCode == 200) {
+      final bodyAsJson = jsonDecode(response.data!) as JsonMap;
+      if (bodyAsJson case {"results": final List<dynamic> results}) {
+        return Dict({
+          "results": results
+              .map((result) => JsonMap(result as Dict<dynamic>))
+              .toList(),
+        });
       }
-    } catch (e) {
-      print(e);
     }
 
     return Dict({"results": []});
   }
-
-  // @override
-  // Future<List<Genre>> get genres async {
-  //   try {
-  //     final dataAsJson = jsonDecode(genresResponseString);
-
-  //     if (dataAsJson case final List<dynamic> genres) {
-  //       return genres.map((item) => Genre.fromJson(item as JsonMap)).toList();
-  //     }
-  //     // final response =
-  //     //     await http.get(Uri.parse('$_baseUrl/genres/?apiKey=$_apiKey'));
-  //     // if (response.statusCode == 200) {
-  //     //   final bodyAsJson = jsonDecode(utf8.decode(response.bodyBytes));
-
-  //     //   if (bodyAsJson case final List<Map<String, dynamic>> list) {
-  //     //     return list.map((item) => Genre.fromJson(item as JsonMap)).toList();
-  //     //   }
-  //     // }
-
-  //     debugPrint('Some error occurred');
-  //     return const <Genre>[];
-  //   } finally {
-  //     notifyListeners();
-  //   }
-  // }
 
   // @override
   // Future<List<Source>> get sources async {
@@ -148,51 +69,30 @@ final class WatchModeApiProvider extends ChangeNotifier {
   //   }
   // }
 
-  // @override
-  // Future<TitleDetails> titleDetails(int id) async {
-  //   try {
-  //     final dataAsJson = jsonDecode(titleDetailsResponse);
+  Future<TitleDetails> titleDetails(int id) async {
+    final response =
+        await _client.get<String>('/title/$id/details/?apiKey=$_apiKey');
+    if (response.statusCode == 200) {
+      final bodyAsJson = jsonDecode(response.data!) as JsonMap;
 
-  //     return TitleDetails.fromJson(dataAsJson as JsonMap);
-  //     // final response = await http
-  //     //     .get(Uri.parse('$_baseUrl/title/$id/details/?apiKey=$_apiKey'));
-  //     // if (response.statusCode == 200) {
-  //     //   final bodyAsJson =
-  //     //       jsonDecode(utf8.decode(response.bodyBytes)) as JsonMap;
+      return TitleDetails.fromMap(bodyAsJson);
+    } else {
+      throw Exception();
+    }
+  }
 
-  //     //   return TitleDetails.fromJson(bodyAsJson);
-  //     // } else {
-  //     //   throw Exception();
-  //     // }
-  //   } finally {
-  //     notifyListeners();
-  //   }
-  // }
+  Future<List<TitleSource>> titleSources(int id) async {
+    final response =
+        await _client.get<String>('/title/$id/sources/?apiKey=$_apiKey');
+    if (response.statusCode == 200) {
+      final bodyAsList = jsonDecode(response.data!) as List<dynamic>;
+      final sourcesAsListOfMap = bodyAsList.cast<JsonMap>();
 
-  // @override
-  // Future<List<TitleSource>> titleSources(int id) async {
-  //   try {
-  //     final dataAsJson = jsonDecode(titleSourcesResponse) as List<dynamic>;
-
-  //     return <TitleSource>[
-  //       for (final item in dataAsJson) TitleSource.fromJson(item as JsonMap),
-  //     ];
-  //     // final response = await http
-  //     //     .get(Uri.parse('$_baseUrl/title/$id/sources/?apiKey=$_apiKey'));
-  //     // if (response.statusCode == 200) {
-  //     //   final bodyAsJson =
-  //     //       jsonDecode(utf8.decode(response.bodyBytes)) as List<dynamic>;
-
-  //     //   final sourcesAsListOfMap = bodyAsJson.cast<JsonMap>();
-
-  //     //   return <TitleSource>[
-  //     //     for (final sourceMap in sourcesAsListOfMap)
-  //     //       TitleSource.fromJson(sourceMap),
-  //     //   ];
-  //     // }
-  //     return const <TitleSource>[];
-  //   } finally {
-  //     notifyListeners();
-  //   }
-  // }
+      return <TitleSource>[
+        for (final sourceMap in sourcesAsListOfMap)
+          TitleSource.fromMap(sourceMap),
+      ];
+    }
+    return const <TitleSource>[];
+  }
 }
