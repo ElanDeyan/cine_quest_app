8aee975d04f134a650286306c5ce035c55928fcb
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-07-27 00:32:11 -0300

    feat: adds region to source options and small fixes

--- lib/screens/media_details/media_details_body.dart
+++ lib/screens/media_details/media_details_body.dart
@@ -22,80 +22,105 @@ class TitleDetailsBody extends StatelessWidget {
         padding: const EdgeInsets.only(
           bottom: kFloatingActionButtonMargin + kBottomNavigationBarHeight,
         ),
-        child: Column(
-          children: [
-            const _MovieDetailsHeader(),
-            const SizedBox(
-              height: 92.5 + 10,
-            ),
-            Text(
-              titleDetails.title,
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (titleDetails.year != null)
-                  Text(
-                    titleDetails.year.toString(),
-                  ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const _MovieDetailsHeader(),
+              const SizedBox(
+                height: 92.5 + 10,
+              ),
+              Text(
+                titleDetails.title,
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              if (titleDetails.title != titleDetails.originalTitle) ...[
+                Text(
+                  titleDetails.originalTitle,
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
                 const SizedBox(
-                  width: 20,
+                  height: 10,
                 ),
-                if (titleDetails.criticScore != null)
-                  IconWithLabel(
-                    icon: const Icon(Icons.grade_outlined),
-                    label: Text(titleDetails.criticScore.toString()),
-                    gap: 5,
-                  ),
               ],
-            ),
-            if (titleDetails.sources.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: FilledButton.icon(
-                  onPressed: () => showDialog<void>(
-                    context: context,
-                    builder: (context) => SimpleDialog(
-                      title: const Text('Escolha sua plataforma'),
-                      children: [
-                        for (final source in titleDetails.sources)
-                          SimpleDialogOption(
-                            child: Text(source.name),
-                            onPressed: () async {
-                              final platformUrl =
-                                  switch ((isWeb, isAndroid, isIOS)) {
-                                (true, _, _) => source.webUrl,
-                                (_, true, _) => source.androidUrl,
-                                (_, _, true) => source.iosUrl,
-                                _ => source.webUrl,
-                              };
-
-                              if (isNotBlank(platformUrl) &&
-                                  platformUrl!.isURL() &&
-                                  await canLaunchUrl(Uri.parse(platformUrl))) {
-                                await launchUrl(Uri.parse(platformUrl));
-                              }
-                            },
-                          ),
-                      ],
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  if (titleDetails.year != null)
+                    Text(
+                      titleDetails.year.toString(),
                     ),
+                  const SizedBox(
+                    width: 20,
                   ),
-                  label: const Text('Assistir agora'),
-                  icon: const Icon(Icons.play_arrow_outlined),
+                  if (titleDetails.criticScore != null)
+                    IconWithLabel(
+                      icon: const Icon(Icons.grade_outlined),
+                      label: Text(titleDetails.criticScore.toString()),
+                      gap: 5,
+                    ),
+                ],
+              ),
+              if (titleDetails.sources.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: FilledButton.icon(
+                    onPressed: () => showDialog<void>(
+                      context: context,
+                      builder: (context) => SimpleDialog(
+                        title: const Text('Escolha sua plataforma'),
+                        children: [
+                          for (final source in titleDetails.sources)
+                            SimpleDialogOption(
+                              child: Text('${source.name} (${source.region})'),
+                              onPressed: () async {
+                                final platformUrl =
+                                    switch ((isWeb, isAndroid, isIOS)) {
+                                  (true, _, _) => source.webUrl,
+                                  (_, true, _) => source.androidUrl,
+                                  (_, _, true) => source.iosUrl,
+                                  _ => source.webUrl,
+                                };
+
+                                if (isNotBlank(platformUrl) &&
+                                    platformUrl!.isURL() &&
+                                    await canLaunchUrl(
+                                      Uri.parse(platformUrl),
+                                    )) {
+                                  await launchUrl(Uri.parse(platformUrl));
+                                }
+                              },
+                            ),
+                        ],
+                      ),
+                    ),
+                    label: Text(
+                      'Assistir agora',
+                      style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.bodyMedium!.fontSize,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    icon: const Icon(Icons.play_arrow_outlined),
+                  ),
+                ),
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 600),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    titleDetails.plotOverview,
+                    softWrap: true,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
                 ),
               ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Text(
-                titleDetails.plotOverview,
-                softWrap: true,
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
